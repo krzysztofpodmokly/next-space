b54cba71e507d5c7450da47b61afb4a98b63e4b9
kriss <krzysztof.podmokly@gmail.com>
2025-02-07 13:44:26 +0100

    Prisma auth adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
+import { prisma } from "@/lib/prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 });
